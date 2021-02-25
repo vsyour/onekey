bf56432e4174ac4fd6b2395881fadda8bd4e91da
一键安装脚本 <vsyour@qq.com>
2021-02-25 14:03:15 +0800

    Update local-dash.sh

--- local-dash.sh
+++ local-dash.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
+# ver.0.5
 # from https://github.com/doristeo/SwarmBeeBzzz
 
 export COLOR_NC='\e[0m' # No Color
 export COLOR_GREEN='\e[0;32m'
 export COLOR_RED='\e[0;31m'
-
-echo -n "Bee version " ; bee version 
-echo ""
-echo "Your address:"
-curl -s localhost:1635/addresses | jq .ethereum
-echo "Your chequebook:"
-curl -s localhost:1635/chequebook/address | jq .chequebookaddress
+echo -n "Bee version " ; bee version
 echo ""
 if pgrep -x "bee" > /dev/null
 then
+    echo "Your address:"
+    curl -s localhost:1635/addresses | jq .ethereum
+    echo "Your chequebook:"
+    curl -s localhost:1635/chequebook/address | jq .chequebookaddress
+    echo ""
     echo -e "Bee node is ${COLOR_GREEN}running :)${COLOR_NC}"
-    echo -n "Active peers: "; curl -s http://localhost:1635/peers | jq '.peers | length'
+    systemctl status bee | grep 'Active:'
+    echo -n "Peers now: "; curl -s http://localhost:1635/peers | jq '.peers | length'
 else
     echo -e "Bee node is ${COLOR_RED}NOT RUNNING! :(${COLOR_NC}"
+    read -p "Do you want try to start node? (Yy/n)" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      systemctl start bee
+      echo "Please, wait 30sec and run this utility again..."
+      echo "Good Beee!"
+      echo "Bzzz..."
+      exit 0
+    fi
 fi
 echo ""
 PS3='Please enter your choice:'
@@ -36,14 +47,11 @@ do
         "chequebook --gBzz--> node")
             echo "Move gBzz from cheque book to address of node..."
             curl -XPOST -s localhost:1635/chequebook/withdraw\?amount\=1000 | jq
-            
             ;;
         "chequebook <--gBzz-- node")
             echo "Move gBzz from node's address to cheque book..."
             curl -XPOST -s localhost:1635/chequebook/deposit\?amount\=1000 | jq
-            
             ;;
-
         "Quit")
             break
             ;;
